02627f991c1a29ee1b302938a3efc1076f55219d
Nicholas Eli <nicholaseli@nicholas.local>
2022-06-20 21:02:51 -0700

    Update nav item spacing

--- assets/script.js
+++ assets/script.js
@@ -22,6 +22,9 @@
 		}
 	};
 
+	document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
+	if (Shopify.designMode) document.documentElement.classList.add('shopify-design-mode');
+
 	window.onload = function () {
 		console.log('--Initializing Scripts');
 		MobileMenu();
