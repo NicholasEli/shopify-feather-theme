538b83f2982eb41f4b947a0bbefa6b1ebe1373c0
Nicholas Eli <nicholaseli@nicholas.local>
2022-06-20 17:43:41 -0700

    Move index to be dynamic

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,8 @@
 import { MobileMenu } from './MobileMenu.js';
 
+document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
+if (Shopify.designMode) document.documentElement.classList.add('shopify-design-mode');
+
 window.onload = function () {
 	console.log('--Initializing Scripts');
 	MobileMenu();
