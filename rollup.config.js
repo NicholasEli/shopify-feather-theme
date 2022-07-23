08aff7602aa24edd96f6868d5c037b46e3b00433
NicholasEli <nicholasbydesign@gmail.com>
2022-07-22 18:17:10 -0700

    Add source maps

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,12 @@
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: './scripts/index.js',
-  output: [{ file: './assets/script.min.js', format: 'iife', plugins: [terser()] }],
+  plugins: [sourcemaps()],
+  output: [
+    { file: './assets/script.min.js', format: 'iife', sourcemap: true, plugins: [terser()] },
+  ],
   plugins: [nodeResolve()],
 };
--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 import { navigation } from './navigation.js';
 import { search } from './search.js';
 
-document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
-if (Shopify.designMode) document.documentElement.classList.add('shopify-design-mode');
-
 window.onload = async function () {
 	console.log('--Initializing Scripts');
+
+	document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
+	if (Shopify.designMode) document.documentElement.classList.add('shopify-design-mode');
 
 	navigation();
 	search();
