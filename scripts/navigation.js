3d76826ca77d70d027cc1b3680af97980cb8b8e8
NicholasEli <nicholasbydesign@gmail.com>
2022-07-22 18:56:46 -0700

    Add listener to sublinks on mobile

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,3 +1,7 @@
+import { getCSSVariable } from './utils.js';
+
+const md = parseInt(getCSSVariable('--md'));
+
 const clearNavItems = function () {
 	const navItems = document.querySelectorAll('[data-nav-item]');
 	navItems.forEach((navItem) => navItem.classList.remove('feather-header__dropdown-item--active'));
@@ -8,23 +12,22 @@ const clearNavItems = function () {
 const clearDropdowns = function () {
 	const dropdowns = document.querySelectorAll('.feather-header__dropdown');
 
-	dropdowns.forEach((dropdown) => {
-		dropdown.addEventListener('mouseenter', () => console.log('enter'));
-		dropdown.addEventListener('mouseleave', () => clearNavItems());
-	});
+	dropdowns.forEach((dropdown) => dropdown.addEventListener('mouseleave', () => clearNavItems()));
 };
 
 const displayNavItems = function () {
 	const navItems = document.querySelectorAll('[data-nav-item]');
 
 	navItems.forEach((navItem) => {
-		navItem.addEventListener('mouseover', () => {
+		const _select = () => {
 			const selector = navItem.getAttribute('data-nav-item');
 			const nav = document.querySelector(`[data-nav="${selector}"]`);
 			clearNavItems();
 			navItem.classList.add('feather-header__dropdown-item--active');
 			nav.classList.add('active');
-		});
+		};
+		navItem.addEventListener('mouseover', () => _select());
+		navItem.addEventListener('click', () => _select());
 	});
 };
 
