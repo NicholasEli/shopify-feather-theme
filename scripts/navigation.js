ad574269443a369c04330f292f37d2f25befedee
NicholasEli <nicholasbydesign@gmail.com>
2022-07-22 19:42:08 -0700

    animate mobile menu

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,4 +1,4 @@
-import { getCSSVariable } from './utils.js';
+import { asyncTimeout, getCSSVariable } from './utils.js';
 
 const state = {
 	active: false,
@@ -9,6 +9,7 @@ const state = {
 
 const active = 'eather-header__dropdown-item--active';
 const md = parseInt(getCSSVariable('--md'));
+const duration = parseInt(getCSSVariable('--animate-duration'));
 
 const clearNavItems = function () {
 	const navItems = document.querySelectorAll('[data-nav-item]');
@@ -43,21 +44,43 @@ const mobileNavBtn = function () {
 	const btn = document.querySelector('[data-nav-btn]');
 	if (!nav || !btn) return;
 
-	btn.addEventListener('click', () => {
+	const bounds = nav.getBoundingClientRect();
+
+	const _close = async () => {
+		nav.classList.add('animate__slideOutLeft');
+
+		await asyncTimeout(duration);
+		nav.classList.remove('animate__slideInLeft');
+		nav.classList.remove('animate__slideOutLeft');
+		state.setActive = false;
+	};
+
+	btn.addEventListener('click', async () => {
 		if (!state.active) {
 			nav.classList.add('animate__slideInLeft');
+			await asyncTimeout(duration);
 			state.setActive = true;
 			return;
 		}
 
-		nav.classList.remove('animate__slideInLeft');
-		state.setActive = false;
+		_close();
+	});
+
+	document.body.addEventListener('click', (e) => {
+		const x = e.clientX;
+		const y = e.clientY;
+
+		if (!state.active) return;
+
+		console.log(bounds.y, bounds.bottom, y);
+		if (y < bounds.top || y > bounds.bottom) {
+			_close();
+		}
 	});
 
 	window.addEventListener('resize', () => {
 		if (window.innerWidth > md && state.active) {
-			nav.classList.remove('animate__slideInLeft');
-			state.setActive = false;
+			_close;
 		}
 	});
 };
