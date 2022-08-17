3c9cfaf5c1f4a456ad6a9e4f83c40209e39477e9
NicholasEli <nicholasbydesign@gmail.com>
2022-08-16 19:09:45 -0500

    Add toggle script

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,7 +1,32 @@
-const applyClasses = function (elements, classes) {
-	if (classes.length === 1) {
-		elements.forEach((element) => element.toggle(classes));
+import { getCSSVariable, asyncTimeout } from './utils.js';
+
+const delay = getCSSVariable('--animate-duration');
+
+const applyClasses = async function (elements, classlist) {
+	if (!classlist || (classlist && !classlist.length)) return;
+
+	const arr = classlist.split(',');
+	const [active, inactive] = arr;
+
+	if (arr.length === 1) {
+		elements.forEach((element) => element.toggle(active));
 		return;
+	}
+
+	for (element of elements) {
+		if (!active) {
+			element.classlist.add(active);
+			return;
+		}
+
+		if (active) {
+			classlist.add(inactive);
+
+			await asyncTimeout(delay);
+
+			classlist.remove(active);
+			classlist.remove(inactive);
+		}
 	}
 };
 
@@ -10,16 +35,14 @@ export const toggle = function () {
 
 	toggles.forEach((toggle) => {
 		const target = toggle.getAttribute('data-toggle');
-		const classes = toggle.getAttribute('data-toggle-class');
-
-		if (!classes || (classes && !classes.length)) return;
+		const classlist = toggle.getAttribute('data-toggle-class');
 
 		if (target) {
 			const elements = document.querySelectorAll(target);
-			applyClasses(elements, classes);
+			applyClasses(elements, classlist);
 			return;
 		}
 
-		applyClasses(toggle, classes);
+		applyClasses(toggle, classlist);
 	});
 };
