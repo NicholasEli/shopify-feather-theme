c07798de9310f2467159052ba5b55fe6cf922831
NicholasEli <nicholasbydesign@gmail.com>
2022-08-19 17:55:29 -0700

    Add mock loading bar listener styles

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,17 +1,58 @@
+import { asyncTimeout } from './utils.js';
+
+let requests = 0;
+
+const setUI = async function (requests) {
+	const el = document.querySelector('[data-loading]');
+	el.setAttribute('data-loading', 'true');
+
+	const increment = 10 / requests;
+	el.style.width = increment + '%';
+
+	const _reset = function () {
+		el.setAttribute('data-loading', 'false');
+		el.style.width = '0%';
+	};
+
+	const _animate = function () {
+		const percentage = parseInt(el.style.width) + increment;
+		setTimeout(() => {
+			if (requests > 0 && requests < 100) {
+				el.style.width = percentage + '%';
+			} else {
+				_reset();
+			}
+		}, 500);
+	};
+
+	_animate();
+
+	if (requests === 0 || !requests) {
+		requests = 0;
+		el.style.width = '100%';
+		await asyncTimeout(200);
+		_reset();
+		return;
+	}
+};
+
 export const loading = function () {
 	if (!window.fetch) return;
-
-	let requests = 0;
 
 	const mock = window.fetch;
 	window.fetch = async function () {
 		requests++;
+		setUI(requests);
+
 		const response = await mock.apply(this, arguments);
+
+		setUI(requests);
+		requests--;
+		setUI(requests);
+
 		if (response && response.status === 200) {
-			requests--;
 			return response;
 		}
-
 		return null;
 	};
 };
