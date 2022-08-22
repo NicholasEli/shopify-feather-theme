abf9a11586a96f17d467d0f330c98c5f15c14769
NicholasEli <nicholasbydesign@gmail.com>
2022-08-22 16:52:16 -0700

    Prevent message newsletter display if success message

--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -6,6 +6,11 @@ const delay = parseInt(getCSSVariable('--animate-duration'));
 export const newsletter = function () {
 	let storage = getStorage();
 
+	console.log(window.Feather);
+	if (window.Feather?.newsletter?.message) {
+		return;
+	}
+
 	if (!storage) modalOpen({ id: 'newsletter' });
 	if (storage && !storage.newsletter) modalOpen(modal);
 };
