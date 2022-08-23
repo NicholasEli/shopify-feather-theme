d42b66b45e89955ac1f5bb6b50c316e4fa1bf6d5
NicholasEli <nicholasbydesign@gmail.com>
2022-08-22 21:22:08 -0700

    Add spacing to containers

--- scripts/newsletter.js
+++ scripts/newsletter.js
@@ -16,7 +16,4 @@ export const newsletter = function () {
 		storage.set({ key: 'newsletter', value: true });
 		return;
 	}
-
-	if (!obj) modalOpen({ id: 'newsletter' });
-	if (obj && !obj.newsletter) modalOpen({ id: 'newsletter' });
 };
