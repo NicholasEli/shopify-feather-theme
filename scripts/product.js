b3ca8c942af4b7c5193618bdcf67cb681cc4a4a3
NicholasEli <nicholasbydesign@gmail.com>
2022-08-23 08:59:44 -0700

    Fix slider errors

--- scripts/product.js
+++ scripts/product.js
@@ -1,13 +1,17 @@
+import Glide from '@glidejs/glide';
+
 const variantSlider = function () {
-	const slider = document.querySelectorAll('[data-variant-slider]');
+	const sliders = document.querySelectorAll('[data-variant-slider]');
 
-	if (!slider || (slider && slider.length == 0)) return;
+	if (!sliders || (sliders && !sliders.length)) return;
 
-	spotlights.forEach((spotlight) => {
+	sliders.forEach((slider) => {
 		const id = slider.getAttribute('data-variant-slider');
 		const instance = new Glide('[data-variant-slider="' + id + '"]', { perView: 1 });
 		instance.mount();
 	});
 };
 
-export const product = function () {};
+export const product = function () {
+	variantSlider();
+};
