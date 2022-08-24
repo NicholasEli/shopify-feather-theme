eb6c1680d6ee898f510061bbaddb5967d5f6fdd1
NicholasEli <nicholasbydesign@gmail.com>
2022-08-24 15:51:29 -0700

    Render content

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,44 @@
+// Utils
+import { loading } from './loading.js';
+import { inView } from './in-view.js';
+
+// Global Components
 import { navigation } from './navigation.js';
 import { search } from './search.js';
-import { inView } from './in-view.js';
+import { select } from './select.js';
+import { toggle } from './toggle.js';
+import { modal } from './modal.js';
+import { tabbedContent } from './tabbed-content.js';
+
+// Page Components
 import { product } from './product.js';
 import { productSpotlight } from './product-spotlight.js';
 import { productFilter } from './product-filter.js';
-import { select } from './select.js';
-import { toggle } from './toggle.js';
-import { loading } from './loading.js';
-import { modal } from './modal.js';
 import { newsletter } from './newsletter.js';
 
 window.onload = async function () {
 	console.log('--Initializing Scripts');
 
-	document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
-	if (Shopify.designMode) document.documentElement.classList.add('shopify-design-mode');
-
-	// Pre UI logic
+	// Utils
 	loading(); // <!-- call first to intercept http requests
+	inView();
+
+	// Global Components
 	navigation();
 	search();
 	select();
 	toggle();
 	modal();
+	tabbedContent();
 
-	// Post UI logic
-	inView();
+	// Page Components
 	newsletter();
 	product();
 	productSpotlight();
 	productFilter(); // <!-- call after all filter, select and toggle methods have been called
+
+	document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
+	if (Shopify.designMode) document.documentElement.classList.add('shopify-design-mode');
 
 	console.log('--Scripts Loaded');
 };
---
+++ scripts/tabbed-content.js
@@ -0,0 +1,8 @@
+const state = {
+	active: false,
+	set setActive(active) {
+		this.active = active;
+	},
+};
+
+export const tabbedContent = function () {};
