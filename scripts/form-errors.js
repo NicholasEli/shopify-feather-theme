f57b0c74437a8b3a2cba97e1b15ffbf611f6f56c
Nicholas Eli <nicholas@Nicholass-MacBook-Pro.local>
2022-10-20 09:50:46 -0700

    Add error class to form

--- scripts/form-errors.js
+++ scripts/form-errors.js
@@ -1,15 +1,29 @@
 import { Notyf } from 'notyf';
 import { options } from './toast.js';
 
+const errorUI = function (error) {
+	const input = document.querySelector(`input[name="${error.form}[${error.field}]"]`);
+	if (!input) return;
+
+	const parent = input.parentElement;
+	if (!parent || (parent && parent.className.indexOf('input') == -1)) return;
+	parent.classList.add('input--error');
+};
+
+const toast = function (error) {
+	const notyf = new Notyf();
+	notyf.error(error.message);
+};
+
 export const formErrors = function () {
 	if (!window.Feather || (window.Feather && !window.Feather.form_errors)) return;
 	if (!window.Feather.form_errors.length) return;
 
 	const { form_errors } = window.Feather;
 	form_errors.forEach((error) => {
-		if (!error.field || !error.message) return;
-		console.error(error);
-		const notyf = new Notyf();
-		notyf.error(error.message);
+		if (!error.form || !error.field || !error.message) return;
+		console.error(error.message);
+		toast(error);
+		errorUI(error);
 	});
 };
