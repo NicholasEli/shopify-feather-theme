fedcec98e462133cbdeb0eac402de5aad3e2bbb1
Nicholas Eli <nicholas@Nicholass-MacBook-Pro.local>
2022-12-21 12:19:07 -0800

    Add slides per view

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -10,15 +10,25 @@ export const swiper = function () {
 		const navigation = _swiper.getAttribute('data-navigation');
 		const scrollbar = _swiper.getAttribute('data-scrollbar');
 		const loop = _swiper.getAttribute('data-loop');
+		const spaceBetweenSlides = _swiper.getAttribute('data-space-between-slides');
+		const slidesPerViewMobile = _swiper.getAttribute('data-slides-per-view-mobile');
+		const slidesPerViewDesktop = _swiper.getAttribute('data-slides-per-view-desktop');
 
 		const config = {
 			direction: 'horizontal',
 			loop: true,
+			slidesPerView: 1,
+			breakpoints: {
+				950: {
+					slidesPerView: 1,
+				},
+			},
 		};
 
 		if (pagination) {
 			config.pagination = {
 				el: '.swiper-pagination',
+				clickable: true,
 			};
 		}
 
@@ -26,6 +36,7 @@ export const swiper = function () {
 			config.navigation = {
 				nextEl: '.swiper-button-next',
 				prevEl: '.swiper-button-prev',
+				clickable: true,
 			};
 		}
 
@@ -38,6 +49,20 @@ export const swiper = function () {
 		if (loop) {
 			config.loop = true;
 		}
+
+		if (spaceBetweenSlides) {
+			config.spaceBetween = parseInt(spaceBetweenSlides);
+		}
+
+		if (slidesPerViewMobile) {
+			config.slidesPerView = parseInt(slidesPerViewMobile);
+		}
+
+		if (slidesPerViewMobile) {
+			config.breakpoints[950].slidesPerView = parseInt(slidesPerViewDesktop);
+		}
+
+		console.log(config);
 
 		new Swiper(`[data-swiper="${id}"]`, config);
 	});
